6b890af1e672e76b042f1d26e5629848c75e501a
Artur Magioli <arturdepaula00@gmail.com>
2025-08-23 17:01:41 -0300

    Consultas SQL e Projections

--- src/main/java/com/ficcao/dslist/controllers/GameListController.java
+++ src/main/java/com/ficcao/dslist/controllers/GameListController.java
@@ -7,9 +7,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ficcao.dslist.dto.GameListDTO;
+import com.ficcao.dslist.dto.GameMinDTO;
 import com.ficcao.dslist.services.GameListService;
+import com.ficcao.dslist.services.GameService;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping(value = "/lists")
@@ -17,10 +20,16 @@ public class GameListController {
     
     @Autowired
     private GameListService gameListService;
-    
+    @Autowired GameService gameService;
+
     @GetMapping
     public List<GameListDTO> findAll() {
         return gameListService.findAll();
     }
     
+    @GetMapping("/{listId}/games")
+    public List<GameMinDTO> findByList(@PathVariable Long listId) {
+        List<GameMinDTO> result = gameService.findByList(listId);
+        return result;
+    }
 }
